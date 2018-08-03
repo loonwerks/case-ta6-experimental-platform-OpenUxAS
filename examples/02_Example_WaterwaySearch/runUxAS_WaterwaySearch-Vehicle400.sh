e7d4d72de82880b797ed8973417fb92b68b1f9fa
Karl Hoech <karl.hoech@rockwellcollins.com>
2018-08-03 12:37:06 -0500

    Add scripts for running distributed example

---
+++ examples/02_Example_WaterwaySearch/runUxAS_WaterwaySearch-Vehicle400.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+SUFFIX="-Vehicle400"
+
+RM_DATAWORK="rm -R ./datawork${SUFFIX}"
+RM_LOG="rm -R ./log${SUFFIX}"
+
+BIN="../../../build/uxas"
+
+mkdir -p RUNDIR_WaterwaySearch${SUFFIX}
+cd RUNDIR_WaterwaySearch${SUFFIX}
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_WaterwaySearch${SUFFIX}.xml
+
