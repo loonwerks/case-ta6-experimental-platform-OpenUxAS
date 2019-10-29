f3f3d5af7d6b491c85ead7aa4647aae52811e2fb
Karl Hoech <karl.hoech@collins.com>
2019-10-29 07:52:37 -0500

    Configure ph2 example UAV and GS instances of UxAS

--- examples/CASE-TA6-Challenge-Problems/ph2_01_WaterwaySearch/runUxAS_WaterwaySearch_GS.sh
+++ examples/CASE-TA6-Challenge-Problems/ph2_01_WaterwaySearch/runUxAS_WaterwaySearch_GS.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RUN_DIR="$(pwd)/RUNDIR_WaterwaySearch_GS"
 
-BIN="${SCRIPT_DIR}../../../build/uxas"
+BIN="${SCRIPT_DIR}/../../../build/uxas"
 
 rm -rf ${RUN_DIR}
 mkdir -p ${RUN_DIR}
